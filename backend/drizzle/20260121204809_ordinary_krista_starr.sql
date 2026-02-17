b2f56adc651fabb602be831261bafac9fcb137b3
forelandmarine <58338138+forelandmarine@users.noreply.github.com>
2026-02-17 15:15:29 +0000

    refresh

--- backend/drizzle/20260121204809_ordinary_krista_starr.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "sea_time_entries" ADD COLUMN "mca_compliant" boolean;--> statement-breakpoint
-ALTER TABLE "sea_time_entries" ADD COLUMN "detection_window_hours" numeric(10, 2);--> statement-breakpoint
-CREATE INDEX "sea_time_entries_mca_compliant_idx" ON "sea_time_entries" USING btree ("mca_compliant");
