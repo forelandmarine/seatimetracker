1ca6b6d71218fa79d4dd9acf0f3b4737c999be49
forelandmarine <58338138+forelandmarine@users.noreply.github.com>
2026-02-19 11:10:31 +0000

    clean

--- backend/drizzle/20260207215552_tidy_blue_blade.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "user" ADD COLUMN "revenuecat_customer_id" text;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "subscription_platform" text;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "trial_ends_at" timestamp with time zone;
